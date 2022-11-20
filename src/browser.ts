6fcf239c96edf3ff2e710bea1b99eb025dd94d95
idanran <idanran@outlook.com>
2022-11-21 01:06:37 +0800

    fix

--- src/browser.ts
+++ src/browser.ts
@@ -1,8 +1,11 @@
 import inspect from 'object-inspect'
+import sc from 'supports-color'
 import Logger from './shared'
 
 Logger.format('o', (value, target) => {
   return inspect(value).replace(/\s*\n\s*/g, ' ')
 })
+
+Logger.targets[0].colors = sc.stdout && sc.stdout.level
 
 export = Logger
