d0f2d574501981c52eb583d750de33817f5cf25f
idanran <idanran@outlook.com>
2022-11-21 14:35:16 +0800

    Update deno.ts

--- src/deno.ts
+++ src/deno.ts
@@ -9,6 +9,6 @@ Logger.format('o', (value, target) => {
     return Deno.inspect(value, { colors: !!target.colors }).replace(/\s*\n\s*/g, ' ')
 })
 
-Logger.targets[0].colors = Deno.noColor ? false : 3
+Logger.targets[0].colors = !Deno.noColor && 3
 
 export = Logger
